3a2a1aa60ef095b00d0a55f32a81e27af075544d
Dom <domgamescompany@gmail.com>
2023-12-21 00:01:53 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+//GitHub - @killerbean3 - SimpleDarkMode-Javascript
+
+//vars
+const toggleBtn = document.querySelector(".btn");
+const articlesContainer = document.querySelector(".articles");
+
+//for when toggle clicked
+toggleBtn.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-theme");
+});
+
+//mapping for data
+const articlesData = articles
+  .map((article) => {
+    const { title, date, length, snippet } = article;
+    //format date
+    const formatDate = moment(date).format("MMMM Do, YYYY");
+
+    return `<article class="post">
+        <h2>${title}</h2>
+        <div class="post-info">
+          <span>${formatDate}</span>
+          <span>${length} min read</span>
+        </div>
+        <p>
+          ${snippet}
+        </p>
+        </article>`;
+  })
+  .join("");
+
+articlesContainer.innerHTML = articlesData;
